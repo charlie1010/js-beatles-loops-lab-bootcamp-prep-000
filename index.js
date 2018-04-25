200fa34d00e7b3594d81413a8635b5e1537420c2
CHARLIE MCCORMICK <chamccor9692@gmail.com>
2018-04-25 16:20:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function theBeatlesPlay(musicians,instruments) {
   for(var array = []; ; 
   
-  array.push(`${musicians} plays ${instruments}`)
+  return array.push(`${musicians} plays ${instruments}`)
 }
